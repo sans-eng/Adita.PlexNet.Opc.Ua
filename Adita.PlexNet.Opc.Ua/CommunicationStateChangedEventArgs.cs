26c9b8977146bdcd6c7ce2e229c9b37e23709735
Setya Adi Kurnia <setya.adi@weq-tech.com>
2025-04-29 17:43:19 +0700

    Add for handling communicatiion state changed event

---
+++ Adita.PlexNet.Opc.Ua/CommunicationStateChangedEventArgs.cs
@@ -0,0 +1,22 @@
+﻿namespace Adita.PlexNet.Opc.Ua
+{
+    /// <summary>  
+    /// Provides data for the communication state changed event.
+    /// </summary>  
+    public class CommunicationStateChangedEventArgs
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CommunicationStateChangedEventArgs"/> class.
+        /// </summary>
+        /// <param name="state">The current communication state.</param>  
+        public CommunicationStateChangedEventArgs(CommunicationState state)
+        {
+            State = state;
+        }
+
+        /// <summary>
+        /// Gets the current communication state.
+        /// </summary>
+        public CommunicationState State { get; }
+    }
+}
