c09c6402d1eec34b5b1134866391d121891a7be9
Setya Adi Kurnia <setya.adi@weq-tech.com>
2025-09-12 11:45:36 +0700

    Added for automatic struct encoding/decoding

---
+++ Adita.PlexNet.Opc.Ua/Annotations/NamespaceAttribute.cs
@@ -0,0 +1,20 @@
+﻿namespace Adita.PlexNet.Opc.Ua.Annotations;
+
+[AttributeUsage(AttributeTargets.Class)]
+public class NamespaceAttribute : Attribute
+{
+    #region Constructors
+    public NamespaceAttribute(string uri)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(uri);
+        Uri = uri;
+    }
+    #endregion Constructors
+
+    #region Public properties
+    public string Uri
+    {
+        get;
+    }
+    #endregion Public properties
+}
---
+++ Adita.PlexNet.Opc.Ua/Annotations/StructMemberAttribute.cs
@@ -0,0 +1,35 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Adita.PlexNet.Opc.Ua.Annotations;
+
+/// <summary>
+/// Marks that a property or a field is the member of a structure.
+/// </summary>
+/// <remarks>
+/// To use this in a field, the field should be marked also using <see cref="ObservablePropertyAttribute"/>
+/// </remarks>
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+public class StructMemberAttribute : Attribute
+{
+    #region Constructors
+    /// <summary>
+    /// Initializes a new instance of <see cref="StructMemberAttribute"/> using specified <paramref name="memberName"/>
+    /// </summary>
+    /// <param name="memberName">The member name of the structure on server side.</param>
+    public StructMemberAttribute(string memberName)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(memberName);
+        MemberName = memberName;
+    }
+    #endregion Constructors
+
+    #region Public properties
+    /// <summary>
+    /// Gets the member name of the structure on server side.
+    /// </summary>
+    public string MemberName
+    {
+        get;
+    } = string.Empty;
+    #endregion Public properties
+}
